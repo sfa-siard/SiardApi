ab20712316a211efa4f2da9fd5a81fb233bc2338
Max Burri <burri@puzzle.ch>
2025-07-23 10:06:32 +0200

    chore: configure jvm toolchain

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,17 @@
 import java.net.URI
 
-plugins {
-    // Apply the foojay-resolver plugin to allow automatic download of JDKs
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-
 rootProject.name = "siard-api"
-include("lib")
 
 
 sourceControl {
     gitRepository(URI.create("https://github.com/sfa-siard/EnterUtilities.git")) {
         producesModule("ch.admin.bar:enterutilities")
     }
+
     gitRepository(URI.create("https://github.com/sfa-siard/SqlParser.git")) {
         producesModule("ch.admin.bar:SqlParser")
     }
+
     gitRepository(URI.create("https://github.com/sfa-siard/Zip64File.git")) {
         producesModule("ch.admin.bar:Zip64File")
     }
