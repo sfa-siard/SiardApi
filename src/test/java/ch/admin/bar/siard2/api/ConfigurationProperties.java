57645f261749f5706d75cb0c2955e9795abfac26
llorentelemmc <llorente@puzzle.ch>
2025-08-11 13:49:40 +0200

    remove ant config

--- src/test/java/ch/admin/bar/siard2/api/ConfigurationProperties.java
+++ src/test/java/ch/admin/bar/siard2/api/ConfigurationProperties.java
@@ -1,33 +1,11 @@
 package ch.admin.bar.siard2.api;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
 import java.util.Properties;
 
-import static org.junit.Assert.fail;
-
-public class ConfigurationProperties
-        extends Properties {
+public class ConfigurationProperties extends Properties {
     private static final long serialVersionUID = 5204423170460249028L;
 
-    private void readProperties() {
-        try {
-            Reader rdr = new FileReader("build.properties");
-            load(rdr);
-            rdr.close();
-        } catch (IOException ie) {
-            fail(ie.getClass()
-                   .getName() + ": " + ie.getMessage());
-        }
-    }
-
-    public ConfigurationProperties() {
-        readProperties();
-    }
-
     public String getLobsFolder() {
-        return getProperty("lobsfolder");
+        return "./build/tmp/test-lobs";
     }
-
 }
