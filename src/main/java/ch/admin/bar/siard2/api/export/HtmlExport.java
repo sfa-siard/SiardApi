23d0f2d1cdfe0788ab753559f7b027383ca7e849
Max Burri <burri@puzzle.ch>
2025-09-10 16:43:11 +0200

    refactor: remove recursion

--- src/main/java/ch/admin/bar/siard2/api/export/HtmlExport.java
+++ src/main/java/ch/admin/bar/siard2/api/export/HtmlExport.java
@@ -35,76 +35,81 @@ public class HtmlExport {
         try (
                 OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8)
         ) {
-            write(folderLobs, outputStreamWriter);
+            write(outputStreamWriter, folderLobs);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
     }
 
-    private void write(File folderLobs, OutputStreamWriter oswr) throws IOException {
-        StringBuilder content = new StringBuilder();
-        content.append("<!DOCTYPE html>\n")
-               .append("<html lang=\"en\">\n")
-               .append("  <head>\n")
-               .append("    <title>")
-               .append(escapeHtml4(metaTable.getName()))
-               .append("</title>\n")
-               .append("    <meta charset=\"utf-8\" />\n")
-               .append("  </head>\n")
-               .append("  <body>\n")
-               .append("    <p>")
-               .append(metaTable.getName())
-               .append("</p>\n")
-               .append("    <p>")
-               .append(metaTable.getDescription())
-               .append("</p>\n")
-               .append("    <table>\n")
-               .append("      <tr>\n");
+    private void write(OutputStreamWriter oswr, File folderLobs) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<!DOCTYPE html>\n")
+          .append("<html lang=\"en\">\n")
+          .append("  <head>\n")
+          .append("    <title>")
+          .append(escapeHtml4(metaTable.getName()))
+          .append("</title>\n")
+          .append("    <meta charset=\"utf-8\" />\n")
+          .append("  </head>\n")
+          .append("  <body>\n")
+          .append("    <p>")
+          .append(metaTable.getName())
+          .append("</p>\n")
+          .append("    <p>")
+          .append(metaTable.getDescription())
+          .append("</p>\n")
+          .append("    <table>\n")
+          .append("      <tr>\n");
 
-        content.append(getColumnHeaders());
-        content.append("      </tr>\n");
-        addRows(folderLobs, content);
-        content.append("    </table>\n");
-        content.append("  </body>\n");
-        content.append("</html>\n");
-        oswr.write(content.toString());
+        sb.append(getColumnHeaders());
+        sb.append("      </tr>\n");
+        sb.append(getRows(folderLobs));
+        sb.append("    </table>\n");
+        sb.append("  </body>\n");
+        sb.append("</html>\n");
+        oswr.write(sb.toString());
         oswr.flush();
     }
 
     private String getColumnHeaders() {
-        return metaTableFacade.getMetaColums().stream()
-                .map(MetaColumn::getName)
-                .map(StringEscapeUtils::escapeHtml4)
-                .map(name -> "        <th>" + name + "</th>\n")
-                .collect(Collectors.joining());
+        return metaTableFacade.getMetaColums()
+                              .stream()
+                              .map(MetaColumn::getName)
+                              .map(StringEscapeUtils::escapeHtml4)
+                              .map(name -> "        <th>" + name + "</th>\n")
+                              .collect(Collectors.joining());
 
     }
 
-    private void addRows(File folderLobs, StringBuilder content) throws IOException {
+    private String getRows(File folderLobs) throws IOException {
+        StringBuilder sb = new StringBuilder();
         for (long rows = 0; rows < metaTable.getRows(); rows++) {
-            content.append("      <tr>\n");
-            addColumns(folderLobs, content);
-            content.append("      </tr>\n");
+            sb.append("      <tr>\n");
+            sb.append(getColumns(folderLobs));
+            sb.append("      </tr>\n");
         }
+        return sb.toString();
     }
 
-    private void addColumns(File folderLobs, StringBuilder content) throws IOException {
-
+    private String getColumns(File folderLobs) throws IOException {
+        StringBuilder sb = new StringBuilder();
 
         new TableRecordFacade(this.dispenser.get()).getCells()
-                                      .forEach(cell -> {
-                                          try {
-                                              content.append("        <td>");
-                                              content.append(writeValue(cell, folderLobs));
-                                              content.append("</td>\n");
-                                          } catch (IOException e) {
-                                              throw new RuntimeException(e);
-                                          }
-                                      });
+                                                   .forEach(cell -> {
+                                                       try {
+                                                           sb.append("        <td>");
+                                                           sb.append(getValue(cell, folderLobs));
+                                                           sb.append("</td>\n");
+                                                       } catch (IOException e) {
+                                                           throw new RuntimeException(e);
+                                                       }
+                                                   });
+
+        return sb.toString();
     }
 
-    private String writeLinkToLob(Value value, File folderLobs, String fileName)
+    private String getLinkToLob(Value value, File folderLobs, String fileName)
             throws IOException {
         StringBuilder sb = new StringBuilder();
         URI absoluteLobFolder = value.getMetaValue()
@@ -152,15 +157,15 @@ public class HtmlExport {
         }
         /* write a link to the LOB file to HTML */
         sb.append("<a href=\"")
-               .append(fileName)
-               .append("\">")
-               .append(fileName)
-               .append("</a>");
+          .append(fileName)
+          .append("\">")
+          .append(fileName)
+          .append("</a>");
 
         return sb.toString();
     }
 
-    private String writeUdtValue( Value value, File folderLobs)
+    private String getUdtValue(Value value, File folderLobs)
             throws IOException {
         StringBuilder sb = new StringBuilder();
         sb.append("<dl>\n");
@@ -171,7 +176,7 @@ public class HtmlExport {
             sb.append(escapeHtml4(mf.getName()));
             sb.append("</dt>\n");
             sb.append("  <dd>");
-            sb.append(writeValue( value.getAttribute(i), folderLobs));
+            sb.append(getValue(value.getAttribute(i), folderLobs));
             sb.append("</dd>\n");
         }
         sb.append("</dl>\n");
@@ -185,40 +190,40 @@ public class HtmlExport {
      * @param folderLobs root folder for internal LOBs in this table.
      * @throws IOException if an I/O error occurred.
      */
-    private String writeArrayValue(Value value, File folderLobs)
+    private String getArrayValue(Value value, File folderLobs)
             throws IOException {
 
         StringBuilder sb = new StringBuilder();
         sb.append("<ol>\n");
         for (int iElement = 0; iElement < value.getElements(); iElement++) {
             sb.append("  <li>");
-            sb.append(writeValue(value.getElement(iElement), folderLobs));
+            sb.append(value.getElement(iElement).convert());
             sb.append("</li>\n");
         }
         sb.append("</ol>\n");
         return sb.toString();
     }
 
-    private String writeValue(Value value, File folderLobs) throws IOException {
+    private String getValue(Value value, File folderLobs) throws IOException {
         StringBuilder sb = new StringBuilder();
         if (value.isNull()) return sb.toString();
 
         // if it is a Lob with a file name then create a link
         String fileName = value.getFilename();
         if (fileName != null) {
-            sb.append(writeLinkToLob(value, folderLobs, fileName));
+            sb.append(getLinkToLob(value, folderLobs, fileName));
             return sb.toString();
         }
 
         MetaValue metaValue = value.getMetaValue();
         MetaType metaType = metaValue.getMetaType();
         if ((metaType != null) && (metaType.getCategoryType() == CategoryType.UDT)) {
-            sb.append(writeUdtValue(value, folderLobs));
+            sb.append(getUdtValue(value, folderLobs));
             return sb.toString();
         }
 
         if (metaValue.getCardinality() > 0) {
-            sb.append(writeArrayValue(value, folderLobs));
+            sb.append(getArrayValue(value, folderLobs));
             return sb.toString();
         }
         sb.append(escapeHtml4(value.convert()));
